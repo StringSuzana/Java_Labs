bcdd45719d8fa136efac7ffe037d3dc9cd705d09
suzana <antolin.suzana@gmail.com>
2021-10-09 17:55:52 +0200

    Initial commit

---
+++ src/main/java/hr/santolin/labone/LaboneApplication.java
@@ -0,0 +1,13 @@
+package hr.santolin.labone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/santolin/labone/LaboneApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.santolin.labone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
